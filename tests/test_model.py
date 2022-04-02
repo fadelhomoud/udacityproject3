a5c416b366a23e14b4dd298e4aed9ccb4df725e4
Fadhel610 <fadhel.h26@gmail.com>
2022-04-02 13:55:33 +0300

    test3 setup

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import pytest
-from ml.model import train_model, compute_model_metrics, inference, slice_performance
+from model import train_model, compute_model_metrics, inference, slice_performance
 
 
 @pytest.fixture
