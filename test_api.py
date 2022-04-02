5ed3c062a0bdc5f33f95cb747f6aa1c80fcdde70
Fadhel610 <fadhel.h26@gmail.com>
2022-04-02 13:38:29 +0300

    test setup

--- test_api.py
+++ test_api.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from MLOPSProject3.main import app
+from main import app
 client = TestClient(app)
 
 
--- testtt.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-catt = os.environ['CATEGORICAL_FEATURES'].split(',')
-print(catt)
-
