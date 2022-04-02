c96b4ac22ea2b97f4bf93eff68a6ef9749d2bf96
Fadhel610 <fadhel.h26@gmail.com>
2022-04-02 17:10:10 +0300

    test4 setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml",
+    version="1.0.0",
+    description="Starter code.",
+    author="Student",
+    install_requires = ['pytest'],
+    packages = find_packages()
+
+)
+
+
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import pytest
-from model import train_model, compute_model_metrics, inference, slice_performance
+from ml.model import train_model, compute_model_metrics, inference, slice_performance
 
 
 @pytest.fixture
